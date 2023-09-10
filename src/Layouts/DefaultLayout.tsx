bed3fa13a3ceded412a030bbf03740fc44f680b4
Rian Lucas <rianluccas08@gmail.com>
2023-09-10 20:04:21 -0300

    feat: make a layout to reuse components

---
+++ src/Layouts/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import Header from "../Components/Header"
+import Footer from "../Components/Footer"
+import { Outlet } from "react-router-dom"
+
+export default function DefaultLayout() {
+  return(
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}

14543100fd52eedf71814f467e534a2484835048
Rian Lucas <rianluccas08@gmail.com>
2023-09-13 19:31:14 -0300

    feat: create Sidebar Component  for About me page

--- src/Components/SidebarAboutMe.tsx
+++ src/Components/SidebarAboutMe.tsx
@@ -1,11 +1,15 @@
 import HobbiesIcon  from '../assets/icons/hobbies-icon.png'
 import PersonalInfo  from '../assets/icons/personal-info-icon.png'
 import ProfessionalInfo  from '../assets/icons/professional-info-icon.png'
+import BioFolder  from '../assets/icons/Vector.png'
+import EducationFolder  from '../assets/icons/EducationFolder.png'
+import InterestsFolder  from '../assets/icons/InterestsFolder.png'
+import FaqSidebar from './aboutMe/FaqSidebar'
 
 export default function SideBarAboutMe() {
   return (
-    <div className='h-home flex'>
-      <div className='px-4 first-line:h-home flex flex-col items-center max-w-[3.75rem] border-r-2  border-r-[#1d2b39]'>
+    <div className='h-home flex font-mono'>
+      <div className='px-4 first-line:h-home flex flex-col items-center max-w-[3.75rem] border-r  border-r-default-border-color'>
         <a href="#">
           <img className='py-4 ' src={ ProfessionalInfo  } alt="" />
         </a>
@@ -19,10 +23,21 @@ export default function SideBarAboutMe() {
         </a>
       </div>
 
-      <div>
-        <h1>
-          <h1>Next Sidebar</h1>
-        </h1>
+      <div className='border-r border-r-default-border-color'>
+
+        <div className='text-white border-b border-default-border-color pb-5'>
+          <details>
+            <summary className='mb-4 pl-5 pr-[5.75rem] py-4 border-b border-default-border-color border-l-0 border-t-0'>
+              <span className='pr-2'>personal-info</span>
+            </summary>
+            
+            <FaqSidebar folder={BioFolder} summaryText='bio' detailsDescription='personal-info'/>
+            <FaqSidebar folder={InterestsFolder} summaryText='interests' detailsDescription='technology'/>
+            <FaqSidebar folder={EducationFolder} summaryText='education' detailsDescription='university'/>
+
+          </details>
+        </div>
+        
       </div>
 
     </div>

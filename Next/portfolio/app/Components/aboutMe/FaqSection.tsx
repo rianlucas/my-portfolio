abb36b7d7041972b39838c561aaa0befa0cb432c
Rian Lucas <rianluccas08@gmail.com>
2023-10-03 20:45:17 -0300

    style: use class nowrap to texts don't jump lines

--- Next/portfolio/app/Components/aboutMe/FaqSection.tsx
+++ Next/portfolio/app/Components/aboutMe/FaqSection.tsx
@@ -8,7 +8,7 @@ export default function FaqSection(props: ContainerProps) {
     <div className='text-default-text-color border-t-0 border-default-border-color'>
       <details className="border-b border-default-border-color align-middle pb-4 mt-4">
         <summary className='pl-5 pr-[5.75rem] border-l-0 border-t-0 text-white transition-all cursor-default'>
-          <span className='pr-2'>{ props.name }</span>
+          <span className='pr-2 whitespace-nowrap'>{ props.name }</span>
         </summary>
 
         <div className="pt-2">
--- Next/portfolio/app/Components/card/IconAndText.tsx
+++ Next/portfolio/app/Components/card/IconAndText.tsx
@@ -9,7 +9,7 @@ export default function IconAndText(props: IconAndTextProps) {
   return (
     <div className="flex pr-5 hover:text-white transition-all">
       <Image className="h-4 mt-1 mr-2 " width={15} height={15} src={props.icon} alt="details"/>
-      <span>{props.text}</span>
+      <span className="whitespace-nowrap">{props.text}</span>
     </div>
   )
 }

a9b853db137f038c62975a9a29eec1738ce8b99f
Rian Lucas <rianluccas08@gmail.com>
2023-10-02 22:22:01 -0300

    fix: put the right name on properties

--- Next/portfolio/app/Components/aboutMe/FoldersInfo.tsx
+++ Next/portfolio/app/Components/aboutMe/FoldersInfo.tsx
@@ -1,10 +1,10 @@
 import MarkDown  from '../../assets/icons/Markdown.png'
 import Link from 'next/link';
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
 
 interface FaqSidebar {
-  folder: string;
+  folder: StaticImageData;
   summaryText: string;
   detailsDescription: string;
 }
@@ -15,7 +15,7 @@ export default function FaqSidebar (props: FaqSidebar) {
       <details>
         <summary className='flex items-center'>
         <svg className="mr-3 w-4 h-4 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
         </svg>
           <i className='pr-2'>
             <Image src={props.folder} alt="" />

216e361f41b68773c3b0987d763eed184e4bbedb
Rian Lucas <rianluccas08@gmail.com>
2023-10-25 22:59:24 -0300

    feat: create projects page

---
+++ Next/portfolio/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import ComingSoon from "../Components/ComingSoon"
+
+export default function section() {
+  return (
+    <div className="h-home flex justify-center items-center">
+      <ComingSoon/>
+    </div>
+  )
+}

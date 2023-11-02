0b80c189a0e561995ebeab4721243796a9ab13fb
Rian Lucas <rianluccas08@gmail.com>
2023-11-02 19:37:36 -0300

    feat: header links change style when is clicked

--- Next/portfolio/app/Components/Header.tsx
+++ Next/portfolio/app/Components/Header.tsx
@@ -1,27 +1,32 @@
-import HeaderLinks from "./HeaderLinks"
-import Link from "next/link"
+'use client';
+import HeaderLinks from "./HeaderLinks";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function Header () {
+export default function Header() {
+ const [activeLink, setActiveLink] = useState("");
 
-  return (
-    <div className="flex justify-between max-w-full bg-[#061726] rounded-lg border-solid border-b border-[#1d2b39] text-base font-mono ">
-      <div className="flex box-border">
-        <Link 
-        href="/" 
-        className="px-8 pr-[12rem] py-4 text-[#3b454e] border-solid border-r border-[#1d2b39]  hover:text-white hover:border-b-[#FEA55F] hover:border-b-2 transition duration-150 ease-in-out">
-          rian-lucas
-        </Link>
+ const handleLinkClick = (href: string) => {
+   setActiveLink(href);
+ };
 
-        <div className="flex ">
-          <HeaderLinks href="aboutMe" name="_about-me"/>
-          <HeaderLinks href="/projects" name="_projects"/>
-          <HeaderLinks href="#" name="_articles"/>
-        </div>
+ return (
+   <div className="flex justify-between max-w-full bg-[#061726] rounded-lg border-solid border-b border-[#1d2b39] text-base font-mono ">
+     <div className="flex box-border">
+       <Link
+         href="/"
+         className="px-8 pr-[12rem] py-4 text-[#3b454e] border-solid border-r border-[#1d2b39] hover:text-white hover:border-b-[#FEA55F] hover:border-b-2 transition duration-150 ease-in-out"
+       >
+         rian-lucas
+       </Link>
 
-      </div>
-        <HeaderLinks href="#" name="_contact-me"/>
-
-    </div>
-  )
-
+       <div className="flex ">
+         <HeaderLinks href="/aboutMe" name="_about-me" activeLink={activeLink} onClick={handleLinkClick} />
+         <HeaderLinks href="/projects" name="_projects" activeLink={activeLink} onClick={handleLinkClick} />
+         <HeaderLinks href="/articles" name="_articles" activeLink={activeLink} onClick={handleLinkClick} />
+       </div>
+     </div>
+     <HeaderLinks href="#" name="_contact-me" activeLink={activeLink} onClick={handleLinkClick} />
+   </div>
+ );
 }
--- Next/portfolio/app/Components/HeaderLinks.tsx
+++ Next/portfolio/app/Components/HeaderLinks.tsx
@@ -1,16 +1,23 @@
+'use client';
 import Link from "next/link";
 
 interface headerLinksProps {
-  name: string;
-  href: string;
+ name: string;
+ href: string;
+ activeLink: string;
+ onClick: (href: string) => void;
 }
 
-export default function HeaderLinks (props: headerLinksProps) {
-  return (
-    <Link 
-      href={props.href} 
-      className="whitespace-nowrap z-10 px-8 py-4 border-b-2 border-b-[#061726] text-[#3b454e] border-solid border-r border-[#1d2b39] last:border-l last:border-r-0 hover:text-white hover:border-b-[#FEA55F] hover:border-b-2 active:border-b-[#FEA55F] transition duration-150 ease-in-out">
-      {props.name}
-    </Link>
-  )
+export default function HeaderLinks(props: headerLinksProps) {
+ const isActive = props.activeLink === props.href;
+
+ return (
+   <Link
+     onClick={() => props.onClick(props.href)}
+     href={props.href}
+     className={`whitespace-nowrap z-10 px-8 py-4 border-b-2 border-b-[#061726] text-[#3b454e] border-solid border-r border-[#1d2b39] last:border-l last:border-r-0 hover:text-white hover:border-b-[#FEA55F] hover:border-b-2 transition duration-150 ease-in-out ${isActive ? "text-white border-b-[#FEA55F] border-b-2" : ""}`}
+   >
+     {props.name}
+   </Link>
+ );
 }

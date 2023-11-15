69b72bc93bcf441a5583f32c4c5b05b349b4a7a1
Rian Lucas <rianluccas08@gmail.com>
2023-11-15 14:03:44 -0300

    style: remove width from div

--- Next/portfolio/app/Components/aboutMe/FaqSection.tsx
+++ Next/portfolio/app/Components/aboutMe/FaqSection.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 
 export default function FaqSection(props: ContainerProps) {
   return (
-    <div className='text-default-text-color border-t-0 border-default-border-color w-[16.6rem]'>
+    <div className='text-default-text-color border-t-0 border-default-border-color'>
       <details className="border-b border-default-border-color align-middle pb-4 mt-4">
         <summary className='pl-5 pr-[5.75rem] border-l-0 border-t-0 text-white transition-all cursor-default'>
           <span className='pr-2 whitespace-nowrap'>{ props.name }</span>

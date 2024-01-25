b7f213dae7940a37d903894ccbcede827baf5b37
Rian Lucas <rianluccas08@gmail.com>
2024-01-25 16:08:15 -0300

    style: change checkbox and label

--- Next/portfolio/app/Components/projects/ProjectsOptionsFilter.tsx
+++ Next/portfolio/app/Components/projects/ProjectsOptionsFilter.tsx
@@ -8,9 +8,11 @@ interface ProjectOptionsFilterProps {
 export default function ProjectOptionsFilter (props: ProjectOptionsFilterProps) {
   return (
       <div className="mx-5 my-2 flex">
-        <input type="checkbox" />
+        <input id={props.name} type="checkbox" className="peer relative appearance-none shrink-0 w-4 h-4 border border-[#607B96] rounded-sm mt-1 bg-[default-bg-color] checked:bg-[#607B96] hover:border-sky-900"/>
         <Image className='mx-2' src={props.icon} width={16} height={16} alt='teste'  />
-        <p> {props.name} </p>
+        <label htmlFor={props.name} className="font-fira-code text-[default-text-color]">
+          {props.name} 
+        </label>
       </div>
   )
 }

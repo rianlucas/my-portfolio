a431e7621852a9c8f24bd4e7eca2a857451790c0
Rian Lucas <rianluccas08@gmail.com>
2024-01-25 15:42:12 -0300

    feat: create sidebar with options to filter projects

--- Next/portfolio/app/projects/page.tsx
+++ Next/portfolio/app/projects/page.tsx
@@ -1,9 +1,37 @@
-import ComingSoon from "../Components/ComingSoon"
+import FaqSection from '../Components/aboutMe/FaqSection'
+import SideBarAboutMe from '../Components/aboutMe/SidebarAboutMe'
+import ProjectOptionsFilter from '../Components/projects/ProjectsOptionsFilter'
 
 export default function section() {
   return (
-    <div className="h-home flex justify-center items-center">
-      <ComingSoon/>
+    <div className="h-home flex">
+      <SideBarAboutMe>
+        <FaqSection name='projects' isOpen>
+          <ProjectOptionsFilter 
+            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg'
+            name='PHP'
+          />
+
+          <ProjectOptionsFilter 
+            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg'
+            name='Docker'
+          />
+
+          <ProjectOptionsFilter 
+            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+            name='MySQL'
+          />
+
+          <ProjectOptionsFilter 
+            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg'
+            name='C#'
+          />
+        </FaqSection>
+      </SideBarAboutMe>
+      
+      <div>
+
+      </div>
     </div>
   )
 }

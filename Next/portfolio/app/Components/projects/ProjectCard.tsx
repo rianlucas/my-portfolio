70827dddaf4a2fac00a92bfd37f8511ada425f4d
Rian Lucas <rianluccas08@gmail.com>
2024-02-02 11:22:09 -0300

    refactor: add icon of techs on project cards

--- Next/portfolio/app/Components/projects/ProjectCard.tsx
+++ Next/portfolio/app/Components/projects/ProjectCard.tsx
@@ -1,39 +1,47 @@
-import Image, { StaticImageData } from "next/image"
-import SubmitButton from "../form/SubmitButton"
+import Image, { StaticImageData } from "next/image";
+import SubmitButton from "../form/SubmitButton";
 
-type ProjectCardProps = { 
-  name: string,
-  number: string,
-  description: string,
-  image: StaticImageData | string,
-}
+type ProjectCardProps = {
+  name: string;
+  number: string;
+  description: string;
+  icon: string;
+  image: StaticImageData | string;
+};
 
 export default function ProjectCard(props: ProjectCardProps) {
   return (
-    <div className='xl:mx-4 2xl:mx-8'>
-          
-    <div className='py-3'>
-      <p className='text-[#5565E8] font-semibold'>Project {props.number}  <span className='text-default-text-color font-normal'>\\ {props.name}</span> </p>
-    </div>
-
-    <div className='border border-default-border-color rounded-md'>
-      <div>
-        <Image alt='imagem' src={props.image} />
+    <div className="xl:mx-4 2xl:mx-8">
+      <div className="py-3">
+        <p className="font-semibold text-[#5565E8]">
+          Project {props.number}{" "}
+          <span className="text-default-text-color font-normal">
+            \\ {props.name}
+          </span>{" "}
+        </p>
       </div>
 
-      <div className='bg-[#011221]'>
-        <p className='p-7 text-default-text-color text-lg'>{props.description}</p>
-        <div className='pb-6 px-7'>
-          <SubmitButton 
-            buttonName='view-project' 
-            link="#"
+      <div className="border-default-border-color relative rounded-md border">
+        <div>
+          <Image
+            alt="imagem"
+            src={props.icon}
+            width={30}
+            height={30}
+            className="absolute right-3 top-3"
           />
+          <Image alt="imagem" src={props.image} />
+        </div>
+
+        <div className="bg-[#011221]">
+          <p className="text-default-text-color p-7 text-lg">
+            {props.description}
+          </p>
+          <div className="px-7 pb-6">
+            <SubmitButton buttonName="view-project" link="#" />
+          </div>
         </div>
       </div>
-
     </div>
-
-
-  </div>
-  )
+  );
 }
--- Next/portfolio/app/projects/page.tsx
+++ Next/portfolio/app/projects/page.tsx
@@ -1,83 +1,88 @@
-import FaqSection from '../Components/aboutMe/FaqSection'
-import SideBarAboutMe from '../Components/aboutMe/SidebarAboutMe'
-import ProjectOptionsFilter from '../Components/projects/ProjectsOptionsFilter'
-import ProjectCard from '../Components/projects/ProjectCard'
-import Rectangle from '../assets/Rectangle 42.png'
+import FaqSection from "../Components/aboutMe/FaqSection";
+import SideBarAboutMe from "../Components/aboutMe/SidebarAboutMe";
+import ProjectOptionsFilter from "../Components/projects/ProjectsOptionsFilter";
+import ProjectCard from "../Components/projects/ProjectCard";
+import Rectangle from "../assets/Rectangle 42.png";
 
 export default function section() {
   return (
     <div className="h-home flex">
       <SideBarAboutMe>
-        <FaqSection name='projects' isOpen>
-          <ProjectOptionsFilter 
-            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg'
-            name='PHP'
+        <FaqSection name="projects" isOpen>
+          <ProjectOptionsFilter
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            name="PHP"
           />
 
-          <ProjectOptionsFilter 
-            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg'
-            name='Docker'
+          <ProjectOptionsFilter
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+            name="Docker"
           />
 
-          <ProjectOptionsFilter 
-            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
-            name='MySQL'
+          <ProjectOptionsFilter
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+            name="MySQL"
           />
 
-          <ProjectOptionsFilter 
-            icon='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg'
-            name='C#'
+          <ProjectOptionsFilter
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+            name="C#"
           />
         </FaqSection>
       </SideBarAboutMe>
-      <div className='grid overflow-y-auto'>
-        <div className='flex justify-between py-2 mx-8 font-fira-code'>
+      <div className="grid overflow-y-auto">
+        <div className="font-fira-code mx-8 flex justify-between py-2">
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            number="1"
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
           />
 
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            number="1"
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
           />
 
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            number="1"
           />
         </div>
 
-        <div className='flex justify-between py-2 mx-8 font-fira-code'>
+        <div className="font-fira-code mx-8 flex justify-between py-2">
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            number="1"
           />
 
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            number="1"
           />
 
           <ProjectCard
-            name='project test'
-            description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+            name="project test"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
             image={Rectangle}
-            number='1'
+            icon="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            number="1"
           />
         </div>
       </div>
-
     </div>
-  )
+  );
 }
